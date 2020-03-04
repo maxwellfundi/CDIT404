456b4fe8b61602b4efe4cf72934d9b71cc38f0de
maxwell fundi <maxwellfundi@live.com>
2020-03-04 20:07:23 +0300

    Additonal changes

--- app/src/main/java/com/farm/myfirstapp/MainActivity.java
+++ app/src/main/java/com/farm/myfirstapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.farm.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import java.io.BufferedOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+            }
 }
