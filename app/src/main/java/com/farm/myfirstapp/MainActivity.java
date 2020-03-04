76b51a47c7e650a51d63c5f04e8de279bed8fb11
maxwell fundi <maxwellfundi@live.com>
2020-03-04 20:44:57 +0300

    Additonal changes - adding buttons to open new activity

--- app/src/main/java/com/farm/myfirstapp/MainActivity.java
+++ app/src/main/java/com/farm/myfirstapp/MainActivity.java
@@ -1,15 +1,32 @@
 package com.farm.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
-import java.io.BufferedOutputStream;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+private Button btnMoveNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMoveNext = findViewById(R.id.next_activity);
+
+        btnMoveNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, activity_2.class);
+                startActivity(intent);
+
+            }
+        });
 
             }
 }
---
+++ app/src/main/java/com/farm/myfirstapp/activity_2.java
@@ -0,0 +1,14 @@
+package com.farm.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
